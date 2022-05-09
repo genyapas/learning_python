1957b2c2a64c0c9ec7e471b93331eade8778f272
Genya <genya@MacBook.fritz.box>
2022-05-09 17:14:18 +0200

    - charge density - linear solver for potential

--- plasma.py
+++ plasma.py
@@ -8,9 +8,31 @@ n0=0                                #number density
 tEnd=50                             #simulation time
 dt=1                                #timestep
 tsteps=int(tEnd/dt )                     #timesteps
+Lapl=np.zeros([M+1,M+1])
+phi=np.zeros([M+1])
 x=np.zeros([tsteps,N])              #coordinates
 x[0]=np.random.uniform(0,L,N)
 vx=np.zeros([tsteps,N])             #velocities
 Nhalf=int(N/2)
 vx[0,0:Nhalf]=np.random.uniform(0,-1,Nhalf)
 vx[0,Nhalf:]=np.random.uniform(1,0,N-Nhalf)
+dx=int(L/M)			    			#delta x
+for i in range(0,M+1):
+	Lapl[i,i]=-2
+    if i!=M:
+		Lapl[i,i+1]=1
+        Lapl[i+1,i]=1
+Lapl[0,M]=1
+Lapl[M,0]=1
+for t in range(tsteps):
+	n=np.zeros([M])		    		#charge density
+	for i in range(N):
+		nl=int(x[t,i]/dx)
+		nr=nl+1
+		if nr==(M+1):
+			nr=0
+		wnl=(x[t,i]-dx*nl)/dx
+		n[nl]=n[nl]+wnl
+		wnr=(dx*nr-x[t,i])/dx
+		n[nr]=n[nr]+wnr
+	phi=np.linalg.solve(Lapl,n)
